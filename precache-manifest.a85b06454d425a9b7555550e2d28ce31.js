906a0b9d9066057d970070ba19da06f08b9da710
cra3e <cra3e@yandex.ru>
2023-05-10 23:08:44 +0300

    Updates

---
+++ precache-manifest.a85b06454d425a9b7555550e2d28ce31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "be05cb22c106f120e164",
+    "url": "/techno/static/css/main.5801e5e8.chunk.css"
+  },
+  {
+    "revision": "be05cb22c106f120e164",
+    "url": "/techno/static/js/main.be05cb22.chunk.js"
+  },
+  {
+    "revision": "8ac703b7e6f4372fecc6",
+    "url": "/techno/static/js/1.8ac703b7.chunk.js"
+  },
+  {
+    "revision": "63d00d42c429591985eb",
+    "url": "/techno/static/js/2.63d00d42.chunk.js"
+  },
+  {
+    "revision": "25a9f705cfc1a2f16aa5",
+    "url": "/techno/static/js/runtime~main.25a9f705.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/techno/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "28ad6f2b4e55038d9cd4271d62174610",
+    "url": "/techno/index.html"
+  }
+];
